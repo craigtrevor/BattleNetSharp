b7e25d3dd0fbd4101dd089bc65c91bb32189b2b7
Craig.Trevor <craig.trevor@gmail.com>
2016-06-29 07:35:18 +1000

    added RewardItem class

---
+++ BattleNetSharp.Community/Wow/RewardItem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleNetSharp.Community.Wow
+{
+    /// <summary>
+    ///   Represents an achievement reward item
+    /// </summary>
+    [DataContract]
+    public class RewardItem
+    {
+        /// <summary>
+        ///   Gets or sets the item id
+        /// </summary>
+        [DataMember(Name = "id", IsRequired = true)]
+        public int Id { get; internal set; }
+
+        /// <summary>
+        /// Reward's item level
+        /// </summary>
+        [DataMember(Name = "itemLevel", IsRequired = false)]
+        public int ItemLevel { get; internal set; }
+
+        /// <summary>
+        ///   Gets or sets the item's stats
+        /// </summary>
+        [DataMember(Name = "stats", IsRequired = false)]
+        public IList<ItemStat> Stats { get; internal set; }
+
+        /// <summary>
+        ///   Gets or sets the item's base armor
+        /// </summary>
+        [DataMember(Name = "armor", IsRequired = true)]
+        public int Armor { get; internal set; }
+
+        /// <summary>
+        ///   Gets or sets the item name
+        /// </summary>
+        [DataMember(Name = "name", IsRequired = true)]
+        public string Name { get; internal set; }
+
+        /// <summary>
+        ///   Gets or sets the item icon
+        /// </summary>
+        [DataMember(Name = "icon", IsRequired = true)]
+        public string Icon { get; internal set; }
+
+        /// <summary>
+        ///   Gets or sets the item's quality
+        /// </summary>
+        [DataMember(Name = "quality", IsRequired = true)]
+        public ItemQuality Quality { get; internal set; }
+
+        /// <summary>
+        ///   Gets or sets the item parameters
+        /// </summary>
+        [DataMember(Name = "tooltipParams", IsRequired = false)]
+        public EquippedItemParameters TooltipParameters { get; internal set; }
+
+        /// <summary>
+        ///   Gets string representation (for debugging purposes)
+        /// </summary>
+        /// <returns> Gets string representation (for debugging purposes) </returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
