d09aa460df744420f7c778be24be096ad9bb120c
Craig.Trevor <craig.trevor@gmail.com>
2016-06-29 18:21:55 +1000

    create BattlegroupTests Unit Test

---
+++ BattleNetSharp.UnitTests/Wow/BattlegroupsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BattleNetSharp.Community.Wow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleNetSharp.UnitTests.Wow
+{
+    [TestClass]
+    public class BattlegroupsTests
+    {
+        [TestMethod]
+        [TestCategory("WOW")]
+        public void TestBattlegroups()
+        {
+            var client = new WowClient(TestConstants.TestRegion, Properties.Settings.Default.PublicKey, TestConstants.TestLocale);
+            var battlegroups = client.GetBattlegroupsAsync().Result;
+            Assert.IsNotNull(battlegroups);
+        }
+    }
+}
