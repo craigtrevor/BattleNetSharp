579310882d244eb543a976687dbe8c227e49b669
Craig.Trevor <craig.trevor@gmail.com>
2016-07-14 07:59:21 +1000

    added ChallengeTests Unit Test

---
+++ BattleNetSharp.UnitTests/Wow/ChallengeTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (C) 2011 by Sherif Elmetainy (Grendiser@Kazzak-EU)
+// Copyright (C) 2016 by Craig Trevor
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using BattleNetSharp.Community.Wow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleNetSharp.UnitTests.Wow
+{
+    [TestClass]
+    public class ChallengeModeTests
+    {
+        private const string TestChallengeModeRealm = "medivh";
+
+        [TestMethod]
+        [TestCategory("WOW.ChallengeMode")]
+        public void TestChallengeMode()
+        {
+            var client = new WowClient(TestConstants.TestRegion, Properties.Settings.Default.PublicKey, TestConstants.TestLocale);
+            var challengeMode = client.GetChallengeLeadersAsync(TestChallengeModeRealm).Result;
+            Assert.IsNotNull(challengeMode);
+            challengeMode = client.GetChallengeLeadersAsync(string.Empty).Result;
+            Assert.IsNotNull(challengeMode);
+        }
+    }
+}
